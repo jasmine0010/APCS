99c0bd8321fd02ff593ff696844545094d206286
JaaSmiNe100 <jasminewu2018@outlook.com>
2024-09-11 12:07:01 -0700

    Multiples Of 3

---
+++ unit0/MultiplesOf3.java
@@ -0,0 +1,9 @@
+public class MultiplesOf3 {
+    public static void main(String[] args) {
+        
+        for (int i=0; i<30; i+=3) {
+            System.out.println(i);
+        }
+        
+    }
+}
